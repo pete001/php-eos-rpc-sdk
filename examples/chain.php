b44b9414c62efe6ef74a051260ea41f58530beb4
DongJin Seo <alienzin@gmail.com>
2018-08-21 17:35:02 +0900

    Add an example of getRawCodeAndAbi()

--- examples/chain.php
+++ examples/chain.php
@@ -13,6 +13,7 @@ echo $api->getTableRows("eosio", "eosio", "producers", ["limit" => 10]) . PHP_EO
 echo $api->getCurrencyBalance("eosio.token", "eosdacserver") . PHP_EOL;
 echo $api->getCurrencyStats("eosio.token", "EOS") . PHP_EOL;
 echo $api->getAbi("eosio.token") . PHP_EOL;
+echo $api->getRawCodeAndAbi("eosio.token") . PHP_EOL;
 echo $api->getProducers(10) . PHP_EOL;
 echo $api->abiJsonToBin("eosio.token", "transfer", ["blockmatrix1", "blockmatrix1", "7.0000 EOS", "Testy McTest"]) . PHP_EOL;
 echo $api->abiBinToJson("eosio.token", "transfer", "10babbd94888683c10babbd94888683c701101000000000004454f53000000000c5465737479204d6354657374") . PHP_EOL;
